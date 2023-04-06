421a2d9e709026f0e37d39d0538b20492c2d20ed
31102 <>
2023-04-06 04:59:54 +0000

    if와 else에 대해 알아보기

--- .ccls-cache/@home@runner@20230322/and.c
+++ .ccls-cache/@home@runner@20230322/and.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
+// #include<stdio.h>
 
-main(){
-  int a=15;
-  int b=20;
-  int c=a&b;
-  printf(" AND연산 결과 :%d",c);
-}
+// main(){
+//   int a=15;
+//   int b=20;
+//   int c=a&b;
+//   printf(" AND연산 결과 :%d",c);
+// }
--- .ccls-cache/@home@runner@20230322/or.c
+++ .ccls-cache/@home@runner@20230322/or.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
+// #include<stdio.h>
 
-main(){
-  int a=15;
-  int b=20;
-  int c=a|b;
-  printf(" OR연산 결과 :%d",c);
-}
+// main(){
+//   int a=15;
+//   int b=20;
+//   int c=a|b;
+//   printf(" OR연산 결과 :%d",c);
+// }
